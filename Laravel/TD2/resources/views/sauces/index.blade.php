dd56dcf9678f563f1450cfede5eb25ce15fe41a6
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2025-02-19 21:43:32 +0100

    Ajout des fichiers des sauces + correctif paths des images

--- Laravel/TD2/resources/views/sauces/index.blade.php
+++ Laravel/TD2/resources/views/sauces/index.blade.php
@@ -5,7 +5,7 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
             @foreach($sauces as $sauce)
                 <div class="text-center">
-                    <img src="{{ $sauce->imageUrl }}" alt="{{ $sauce->name }}" class="mx-auto h-40">
+                    <img src="{{ asset('storage/' . $sauce->imageUrl) }}" alt="{{ $sauce->name }}" class="mx-auto h-40">
                     <h3 class="mt-2 font-semibold">{{ strtoupper($sauce->name) }}</h3>
                     <p class="text-gray-500">Heat: {{ $sauce->heat }}/10</p>
                     <p class="text-sm text-gray-400">{{ $sauce->manufacturer }}</p>
